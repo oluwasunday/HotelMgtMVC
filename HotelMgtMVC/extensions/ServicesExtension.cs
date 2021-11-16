d023cc7da93ad036623479b8cb96f93b78c229db
Oladejo Sunday <sundayoladejo13@gmail.com>
2021-11-17 00:32:26 +0100

    fetching data from API

---
+++ HotelMgtMVC/extensions/ServicesExtension.cs
@@ -0,0 +1,22 @@
+﻿using HotelMgtServices.implementations;
+using HotelMgtServices.interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HotelMgtMVC.extensions
+{
+    public static class ServicesExtension
+    {
+        public static void AddDependencyInjection(this IServiceCollection services)
+        {
+
+            // Add Repository Injections Here 
+            services.AddSingleton<IHttpRequestFactory, HttpRequestFactory>();
+            services.AddTransient<IAuthenticationService, AuthenticationService>();
+
+
+            // Add Fluent Validator Injections Here
+            //services.AddTransient<IValidator<RegisterDto>, RegisterDtoValidator>();
+
+        }
+    }
+}
---
+++ HotelMgtModel/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
+
+namespace HotelMgtModel.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required (ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Enter a valid Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter Password To Continue")]
+        public string Password { get; set; }
+        public string Id { get; set; }
+        public string Token { get; set; }
+        public Claim Claim { get; set; }
+    }
+}
