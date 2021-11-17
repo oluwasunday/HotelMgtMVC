adf9d4b7cc96f0cb9218a693c2a86727d5716bff
Oladejo Sunday <sundayoladejo13@gmail.com>
2021-11-17 19:49:19 +0100

    contact data consumed

---
+++ HotelMgtServices/implementations/ContactService.cs
@@ -0,0 +1,31 @@
+﻿using HotelMgtModel.Dtos;
+using HotelMgtModel.Utilities;
+using HotelMgtModel.ViewModels;
+using HotelMgtServices.interfaces;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMgtServices.implementations
+{
+    public class ContactService : IContactService
+    {
+        private readonly IHttpRequestFactory _requestFactory;
+
+        public ContactService(IHttpRequestFactory requestFactory)
+        {
+            _requestFactory = requestFactory;
+        }
+
+        public async Task<AddContactViewModel> AddContactUsAsync(AddContactUsDto contactUsDto)
+        {
+            var response = await _requestFactory.PostRequestAsync<AddContactUsDto, Response<AddContactViewModel>>(
+                    requestUrl: $"api/ContactUs/", contactUsDto);
+
+            return response.Data;
+        }
+    }
+}
---
+++ HotelMgtServices/interfaces/IContactService.cs
@@ -0,0 +1,11 @@
+﻿using HotelMgtModel.Dtos;
+using HotelMgtModel.ViewModels;
+using System.Threading.Tasks;
+
+namespace HotelMgtServices.interfaces
+{
+    public interface IContactService
+    {
+        Task<AddContactViewModel> AddContactUsAsync(AddContactUsDto contactUsDto);
+    }
+}
